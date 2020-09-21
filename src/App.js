f02d9f5dc76602449137ec1f84c1f0eb3bb1b4a5
jingfreeks <lyndell.dobluis@gmail.com>
2020-09-21 23:08:33 +0800

    add and update header and options

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import {Switch,Route} from 'react-router-dom';
 import HomePage from './Page/HomePage';
 import ShopPage from './Page/Shop';
+import Header from './Components/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route  path='/shop' component={ShopPage} />
