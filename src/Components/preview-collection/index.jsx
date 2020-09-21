3aa96a0325ad97c8d993f41af23f145aacd1ce69
jingfreeks <lyndell.dobluis@gmail.com>
2020-09-21 22:28:45 +0800

    update app with shop page with collection Items

--- src/Components/preview-collection/index.jsx
+++ src/Components/preview-collection/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './styles.scss';
-
+import CollectionItems from '../collection-item';
 
 const PreviewCollection=({title,items})=>(
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
-            {items.filter((item,idx)=>idx<4).map(item=>(
-                <div key={item.id}>{item.name}</div>
+            {items.filter((item,idx)=>idx<4).map(({id,...othItems})=>(
+                <CollectionItems key={id} {...othItems}/>
             ))}
         </div>
     </div>
